bc28c8d0633c59e29287cf5fc6dc1397bb742a1d
Root lee <54rootlee@gmail.com>
2017-02-16 06:07:33 -0800

    改为Binary格式，解决Windows系统下出现的换行符问题

--- hex2bin.py
+++ hex2bin.py
@@ -1,7 +1,7 @@
 
-input = open('raw3.txt','r')
+input = open('./testfile/raw3.txt','r')
 raw = input.read()
-output = open('bin.dat','w+')
+output = open('./testfile/bin.dat','wb+')
 for i in range(len(raw)/2):
     print chr(int(raw[2*i:2*i+2],16))
     output.write(chr(int(raw[2*i:2*i+2],16)))
